5f4c59f5c5c801e9a0f4744e34328e3044c2dc69
Ambika2510 <kumarambika2510@gmail.com>
2023-08-09 08:48:11 +0530

    name update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -40,7 +40,7 @@ const handlecutsearch=()=>{
   <div className="container  flex flex-wrap items-center justify-between mx-auto">
   <Link to="/" className="flex items-center">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blogosphere</span>
+      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">College Memories</span>
   </Link>
   <div className="flex  w-1/3 lg:order-1">
   
@@ -150,7 +150,7 @@ else{
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="/" className="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blogosphere</span>
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">College Memories</span>
     </a>
     <button  onClick={()=>{setd(1-d)}} type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
