df1b5a4f9fc92f67187743c605948e6bc6a26890
Ambika kumar <95169962+Ambika2510@users.noreply.github.com>
2023-12-09 12:11:44 +0530

    Update Signup.js

--- src/Page/Signup.js
+++ src/Page/Signup.js
@@ -90,7 +90,11 @@ const Signup = () => {
 
   </div>
   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button>
-</form>
+ <h2 className="text-xl mt-6 font-bold text-blue-600">
+              (Try for see demo of website please log into it:- email:rudra20@gmail.com,
+              password:Rudra@321)
+            </h2>
+  </form>
 
     </div>
     </div>
