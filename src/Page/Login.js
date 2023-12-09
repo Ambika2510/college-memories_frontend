bfc81b54f0867c8ef39d3a1db64ac45dd9566290
Ambika2510 <kumarambika2510@gmail.com>
2023-12-09 13:16:23 +0530

    syntax error

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -27,7 +27,6 @@ const Login = () => {
 }
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
       <div className="flex justify-center content-center">
         <div className="w-96 p-6  m-8  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -65,29 +64,6 @@ const Login = () => {
                 required
               />
             </div>
-=======
-    <Navbar/>
-    <div className="flex justify-center content-center">
-    <div className="w-96 p-6  m-8  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
-<form  onSubmit = { handleSubmit }>
-  <div className="mb-6">
-    <label for="email" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="xyz@gmail.com" onChange = {(e) => setEmail(e.target.value)} value = { email }required/>
-  </div>
-  <div className="mb-6">
-    <label for="password" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Your password</label>
-    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange ={(e) => setPassword(e.target.value)} value = { password } required/>
-  </div>
-  
-
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
- <h2 className="text-xl mt-6 font-bold text-blue-600">
-              (Try for see demo of website please log into it:- email:rudra20@gmail.com,
-              password:Rudra@321)
-            </h2>
-  </form>
->>>>>>> 7d8c43c8a439980ba2b6fc2e6cd1f1e74d9c2c97
 
             <button
               type="submit"
--- src/Page/Signup.js
+++ src/Page/Signup.js
@@ -63,7 +63,6 @@ const Signup = () => {
  
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
       <div className="flex justify-center content-center">
         <div className=" p-6  m-8  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -177,47 +176,6 @@ const Signup = () => {
         </div>
       </div>
       <ToastContainer autoClose={40000} />
-=======
-    <Navbar/>
-    <div className="flex justify-center content-center">
-    <div className=" p-6  m-8  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        
-<form onSubmit={handlesubmit}>
-    <div className="grid md:grid-cols-2 md:gap-6">
-<div className="mb-6">
-    <label for="text" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">First name</label>
-    <input type="text" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name"  onChange={(e)=>{setfirstname(e.target.value)}} value={firstname} required/>
-  </div>
-  <div className="mb-6">
-    <label for="text" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Last name</label>
-    <input type="text" id="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="surname" onChange={(e)=>setlastname(e.target.value)} value={lastname} required/>
-  </div>
-  </div>
-  <div className="mb-6">
-    <label for="email" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Your email</label>
-    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" onChange={(e)=>setemail(e.target.value)}value={email} required/>
-  </div>
-  <div className="mb-6">
-    <label for="password" className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Your password</label>
-    <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e)=>setpassword(e.target.value)} value={password} required/>
-  </div>
-  <div className="mb-6">
-    
-
-  <label class="block mb-2 text-lg font-medium text-gray-900 dark:text-white" for="file_input">Upload image</label>
-<input class="block w-full text-lg text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" onChange={(e)=>setfilename(e.target.files[0])} ref={inputref} accept="image/png, image/jpg, image/jpeg" required/>
-<p class="mt-1 text-lg text-gray-500 dark:text-gray-300" id="file_input_help">Only support .png .jpg and .jpeg image file.</p>
-
-
-  </div>
-  <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-lg w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Signup</button>
- <h2 className="text-xl mt-6 font-bold text-blue-600">
-              (Try for see demo of website please log into it:- email:rudra20@gmail.com,
-              password:Rudra@321)
-            </h2>
-  </form>
-
->>>>>>> 7d8c43c8a439980ba2b6fc2e6cd1f1e74d9c2c97
     </div>
   );
 }
